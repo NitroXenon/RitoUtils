34b424eac339d8b864e4464c96cde1b5e13da20e
Francisco <francisco.esteves.leal@gmail.com>
2015-11-02 18:07:29 +0000

    Added annie
    Annie added

---
+++ RitoWars/Logic/Game/Champions/Champs/Annie.cs
@@ -0,0 +1,142 @@
+﻿using RitoWars.Logic.Game.Champions.Helpers;
+
+namespace RitoWars.Logic.Game.Champions.Champs
+{
+    public abstract class Annie : BaseChamp
+    {
+        #region ChampData
+        /// <summary>
+        /// The champion name
+        /// </summary>
+        public override string ChampName => "Annie";
+
+        /// <summary>
+        /// The champion id
+        /// </summary>
+        public override int ChampId => 1;
+
+        #endregion ChampData
+
+        #region Health
+        /// <summary>
+        /// The champion's base health at level one
+        /// </summary>
+        public override double BaseHealth => 511.68;
+
+        /// <summary>
+        /// The champion's health gained for leveling up
+        /// </summary>
+        public override double HealthLevel => 76;
+
+        /// <summary>
+        /// The champion's amount of health regen
+        /// </summary>
+        public override double BaseHealthRegen => 5.42;
+
+        /// <summary>
+        /// The champion's health regen gained for leveling up
+        /// </summary>
+        public override double HealthRegenLevel => 0.55;
+        #endregion Health
+
+        #region SecondaryBar
+        /// <summary>
+        /// The champion's <seealso cref="SecondaryBar"/> resource the champion has
+        /// </summary>
+        public override SecondaryBar SecondaryBar => SecondaryBar.Mana;
+
+        #region SecondaryBarData
+        /// <summary>
+        /// The champion's base <seealso cref="SecondaryBar"/> amount
+        /// </summary>
+        public override double BaseSecondaryBarData => 334;
+
+        /// <summary>
+        /// The champion's <seealso cref="SecondaryBar"/> amount gained for leveling up
+        /// </summary>
+        public override double SecondaryBarDataLevel => 50;
+
+        /// <summary>
+        /// The champion's <seealso cref="SecondaryBar"/> base regen amount
+        /// </summary>
+        public override double BaseSecondaryBarRegen => 6;
+
+        /// <summary>
+        /// The champion's <seealso cref="SecondaryBar"/> regen amount gained for leveling up
+        /// </summary>
+        public override double SecondaryBarRegenLevel => 0.8;
+        #endregion SecondaryBarData
+        #endregion SecondaryBar
+
+        #region Attacks 
+        /// <summary>
+        /// The champion's base attack damage
+        /// </summary>
+        public override double BaseAttackDamage => 50.41;
+
+        /// <summary>
+        /// The champion's attack damage gained for leveling up
+        /// </summary>
+        public override double AttackDamageLevel => 2.625;
+
+        /// <summary>
+        /// The champion's base attack speed
+        /// </summary>
+        public override double BaseAttackSpeed => 0.579;
+
+        /// <summary>
+        /// The champion's attack speed percent gained for leveling up
+        /// </summary>
+        public override double AttackSpeedPercent => 1.00;
+
+        /// <summary>
+        /// The champion's auto attack range
+        /// </summary>
+        public override double AutoAttackRange => 575;
+        #endregion Attacks
+
+        #region Defense
+        /// <summary>
+        /// The champion's base armor
+        /// </summary>
+        public override double BaseArmor => 19.22;
+
+        /// <summary>
+        /// The champion's armor gained for leveling up
+        /// </summary>
+        public override double ArmorLevel => 4;
+
+        /// <summary>
+        /// The champion's base magic resist (for most champions is 30)
+        /// </summary>
+        public override double BaseMagicResist => 30.0;
+
+        /// <summary>
+        /// The champion's magic resist gained for leveling up
+        /// </summary>
+        public override double MagicResistLevel => 0;
+        #endregion Defense
+
+        #region CastingResource
+        /// <summary>
+        /// The champion's <seealso cref="CastingResource"/> for using Q
+        /// </summary>
+        public override CastingResource Q => CastingResource.Mana;
+
+        /// <summary>
+        /// The champion's <seealso cref="CastingResource"/> for using W
+        /// </summary>
+        public override CastingResource W => CastingResource.Mana;
+
+        /// <summary>
+        /// The champion's <seealso cref="CastingResource"/> for using E
+        /// </summary>
+        public override CastingResource E => CastingResource.Mana;
+
+        /// <summary>
+        /// The champion's <seealso cref="CastingResource"/> for using R
+        /// </summary>
+        public override CastingResource R => CastingResource.Mana;
+        #endregion CastingResource
+    }
+}
