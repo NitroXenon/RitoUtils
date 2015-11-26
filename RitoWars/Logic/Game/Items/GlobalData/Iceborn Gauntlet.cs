b2f2cd2abf2fffc00010d9691ebd838b7ff161c4
Francisco <francisco.esteves.leal@gmail.com>
2015-11-26 13:18:55 +0000

    Items
    Added all Items

---
+++ RitoWars/Logic/Game/Items/GlobalData/Iceborn Gauntlet.cs
@@ -0,0 +1,39 @@
+using RitoWars.Logic.Game.Maps;
+
+namespace RitoWars.Logic.Game.Items.GlobalData
+{
+    public class BootsOfSpeed : BaseItem
+    {
+        #region UniqueItemData
+        /// <summary>
+        /// The item id
+        /// </summary>
+        public override int ItemId => 3025;
+        
+        /// <summary>
+        /// The item's name
+        /// </summary>
+        public override string ItemName => "Iceborn Gauntlet";
+
+        /// <summary>
+        /// The target maps
+        /// </summary>
+        //public override MapTypes TargetMap => MapTypes.AllMaps;
+
+        /// <summary>
+        /// The cost to buy the item
+        /// </summary>
+        public override int Cost => 2700;
+
+        /// <summary>
+        /// The max amount of that item type you can buy
+        /// </summary>
+        //public override int ItemLimit => 1;
+#endregion UniqueItemData
+
+        /// <summary>
+        /// The movement speed gained by buying the item
+        /// </summary>
+        //public override double MovementSpeedGained => 25;
+    }
+}
